f53fe2d09e0ded6a58f297983ee4bd7bf9f5a489
lorddef <hugo77630@hotmail.fr>
2014-06-16 11:18:52 +0200

    add page manage report

---
+++ site/javascript/Report.class.js
@@ -0,0 +1,31 @@
+function Report() {}
+
+var attachEventOnButtonsReportSolved = function () {
+    $('.btn-report-solved').click(function() {
+        var reportLine = $(this).parent().parent();
+        $.ajax({
+	   type: "POST",
+	   url: urlServer + ajaxFolderPath + "reportAjax.php",
+	   data:{nameRequest: "reportSolved", idReport: $(this).parents().children('.report-id').text()},
+	   dataType: document.json,
+	   error: function (xhr) { if (xhr.status == 401) alert('Error: ' + xhr.statusText); console.log('error:', xhr.responseText); },
+	   success: function (flag) {
+			if (flag == true)
+                            reportLine.remove();
+                        else
+                            alert("Error: impossible to solved this report.");
+		}
+	});
+        
+    });
+};
+
+Report.displayButtonsReportSolved = function () {
+    $('#table-all-report-thead-tr').prepend('<th>Action</th>');
+    $('.table-all-report-tbody-tr').each( function(){
+        $(this).prepend('<td><button class="btn btn-success btn-report-solved" type="button">Solved</button></td>');
+    });
+    attachEventOnButtonsReportSolved();
+}
+
+attachEventOnButtonsReportSolved();
