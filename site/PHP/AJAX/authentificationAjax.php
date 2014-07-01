8d09d579f72a7d646f2bae062d920047b9033957
root <root@ns3311910.ip-5-135-163.eu>
2014-07-01 03:28:05 +0200

    add missing ajax files

---
+++ site/PHP/AJAX/authentificationAjax.php
@@ -0,0 +1,36 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/MagiSEO/site/PHP/DAO/UserDAO.class.php';
+@session_start();
+header("Content-Type: text/plain");
+
+function	authentification() {
+	$login = isset($_POST['login']) ? ($_POST['login']) : '';
+	$password = isset($_POST['password']) ? ($_POST['password']) : '';
+ 
+        if (!empty($login) && !empty($password) && $user = UserDAO::authentification($login, $password)) {
+            $_SESSION['auth'] = true;
+            $_SESSION['user'] = $user;
+            echo true;
+        }
+        else {
+            echo false;
+            http_response_code(404);
+        }
+}
+
+function disconnection() {
+	$_SESSION['auth'] = null;
+        $_SESSION['user'] = null;
+        echo "good";
+	if (!@session_destroy()) {
+		http_response_code(500);
+	}
+}
+
+if (isset($_POST["nameRequest"])) {
+    if ($_POST["nameRequest"] == "authentification")
+        authentification();
+    else if ($_POST["nameRequest"] == "disconnection")
+        disconnection();
+}
+?>
